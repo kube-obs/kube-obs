b1ddc27c4b4e5661c8b53465a18e68d52f14b982
Yohann <1330242+iamyohann@users.noreply.github.com>
2022-11-17 20:27:09 +1100

    basic UI starter

---
+++ ui/src/pages/api/graphql.ts
@@ -0,0 +1,35 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
+import { gql } from 'graphql-tag';
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  introspection: true,
+  plugins: [
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: 'my-graph-id@my-graph-variant',
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
+});
+
+export default startServerAndCreateNextHandler(server);
